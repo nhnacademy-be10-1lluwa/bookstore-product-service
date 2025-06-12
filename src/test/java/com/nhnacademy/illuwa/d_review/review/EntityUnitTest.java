e116dc3bd2c4c7c2b8dd59f68e76be5681149c7d
JunHyeon <tempjun0215@gmail.com>
2025-06-12 18:19:29 +0900

    refactor: import Assertions.assertThat로 수정

--- src/test/java/com/nhnacademy/illuwa/d_review/review/EntityUnitTest.java
+++ src/test/java/com/nhnacademy/illuwa/d_review/review/EntityUnitTest.java
@@ -7,7 +7,8 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class EntityUnitTest {
     @Test
