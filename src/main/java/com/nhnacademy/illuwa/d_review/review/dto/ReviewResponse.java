7c29cd10584e119e79d8d20000da2b2de71420ab
JunHyeonOh <144202357+JunHyeonOh@users.noreply.github.com>
2025-06-12 12:57:11 +0900

    feat/34-review-controller (#35)
    * feat: ReviewService 및 작성하는데 필요한 예외 추가
    refactor: 리포지토리 수정
    
    * feat: Review 컨트롤러 작성
    refactor: 컨트롤러 작성하면서 dto, service, entity 수정

--- src/main/java/com/nhnacademy/illuwa/d_review/review/dto/ReviewResponse.java
+++ src/main/java/com/nhnacademy/illuwa/d_review/review/dto/ReviewResponse.java
@@ -1,8 +1,8 @@
 package com.nhnacademy.illuwa.d_review.review.dto;
 
+import com.nhnacademy.illuwa.d_review.review.entity.Review;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
@@ -19,4 +19,17 @@ public class ReviewResponse {
     private LocalDateTime reviewDate;
     private Long bookId;
     private Long memberId;
+
+    public static ReviewResponse from(Review review) {
+        return new ReviewResponse(
+                review.getReviewId(),
+                review.getReviewTitle(),
+                review.getReviewContent(),
+                review.getReviewImageUrl(),
+                review.getReviewRating(),
+                review.getReviewDate(),
+                review.getBook().getId(),
+                review.getMemberId()
+        );
+    }
 }
