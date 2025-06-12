bdbab3bc6da55b2f7fd2ccb00cdf18fea8ef0dd8
nhdor <ch@>
2025-06-12 11:16:23 +0900

    chore : BookResponse 네이밍 변경 #30

--- src/main/java/com/nhnacademy/illuwa/d_book/book/dto/BookExternalResponseDto.java
+++ src/main/java/com/nhnacademy/illuwa/d_book/book/dto/BookExternalResponseDto.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class BookAPIResponseDto {
+public class BookExternalResponseDto {
     private String title;
     private String author;
     private String pubDate;
