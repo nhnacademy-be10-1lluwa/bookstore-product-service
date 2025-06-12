4c2467596700abcfa4b695788d42635de35f66fb
nhdor <ch@>
2025-06-12 15:32:45 +0900

    refactor : BookExternalResponse dto 이름 재지정

--- src/main/java/com/nhnacademy/illuwa/d_book/book/service/BookService.java
+++ src/main/java/com/nhnacademy/illuwa/d_book/book/service/BookService.java
@@ -1,6 +1,6 @@
 package com.nhnacademy.illuwa.d_book.book.service;
 
-import com.nhnacademy.illuwa.d_book.book.dto.BookExternalResponseDto;
+import com.nhnacademy.illuwa.d_book.book.dto.BookExternalResponse;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,8 +14,8 @@ public class BookService {
     }
 
     //도서 등록 전 도서 검색
-    public List<BookExternalResponseDto> searchBookFromExternalApi(String title){
-        List<BookExternalResponseDto> bookExternalResponseDtos = aladinBookApiService.searchBooksByTitle(title);
+    public List<BookExternalResponse> searchBookFromExternalApi(String title){
+        List<BookExternalResponse> bookExternalResponseDtos = aladinBookApiService.searchBooksByTitle(title);
 
         return bookExternalResponseDtos;
     }
