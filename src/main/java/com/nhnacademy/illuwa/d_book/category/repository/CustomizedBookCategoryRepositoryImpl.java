5d7bdec9d20b49d0661d9f50e3364cc94b69e2b0
nhdor <ch@>
2025-06-13 11:30:14 +0900

    feat : CustomizedBookCategoryRepositoryImpl 구현 및 findCategoryNamesByBookId 메서드 추가 #46

---
+++ src/main/java/com/nhnacademy/illuwa/d_book/category/repository/CustomizedBookCategoryRepositoryImpl.java
@@ -0,0 +1,31 @@
+package com.nhnacademy.illuwa.d_book.category.repository;
+
+import com.nhnacademy.illuwa.d_book.category.QCategory;
+import com.nhnacademy.illuwa.d_book.category.entity.QBookCategory;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class CustomizedBookCategoryRepositoryImpl implements CustomizedBookCategoryRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<String> findCategoryNamesByBookId(Long bookId) {
+
+        QBookCategory bookCategory = QBookCategory.bookCategory;
+        QCategory category = QCategory.category;
+
+        return queryFactory
+                .select(category.categoryName)
+                .from(bookCategory)
+                .join(bookCategory.category,category)
+                .where(bookCategory.book.id.eq(bookId))
+                .fetch();
+    }
+
+}
