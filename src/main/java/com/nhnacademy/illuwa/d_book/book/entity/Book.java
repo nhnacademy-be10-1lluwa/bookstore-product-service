e46956b3039409cf5d49bf063ba52610560a4f99
JunHyeonOh <144202357+JunHyeonOh@users.noreply.github.com>
2025-06-13 11:03:54 +0900

    test/44-review-unit-test (#49)
    * test: DTO Unit Test
    
    * test: Repository Jpa Test
        - Book Allargsconstructor 필요해서 추가
    
    * refactor: DTO Unit Test - 줄바꿈, RepositoryJpaTest - 가짜 객체 Book id를 10L 로 설정하고 검증값 수정
    
    * refactor: RepositoryJpaTest - 같은 객체 persist 문제 해결
    
    * test: Entity Unit Test 추가
    
    * refactor: import Assertions.assertThat로 수정
    
    * test: ServiceUnitTest 작성
    
    * refactor: 검사 나노초단위 오차 관련 문제 수정

--- src/main/java/com/nhnacademy/illuwa/d_book/book/entity/Book.java
+++ src/main/java/com/nhnacademy/illuwa/d_book/book/entity/Book.java
@@ -1,6 +1,7 @@
 package com.nhnacademy.illuwa.d_book.book.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 @Setter
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -39,5 +41,4 @@ public class Book {
     private int salePrice;
 
     private boolean isGiftWrap;
-
 }
