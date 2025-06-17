55a91aebcf5920d3145842acbcdb52ca84932be4
nhdor <ch@>
2025-06-17 14:23:58 +0900

    feat : bookCategory 생성자 추가 - category와 Book Setter 대신 생성자로 등록

--- src/test/java/com/nhnacademy/illuwa/d_book/book/service/AladinBookApiServiceTest.java
+++ src/test/java/com/nhnacademy/illuwa/d_book/book/service/AladinBookApiServiceTest.java
@@ -1,4 +1,4 @@
-package com.nhnacademy.illuwa.d_book.book.book.service;
+package com.nhnacademy.illuwa.d_book.book.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
