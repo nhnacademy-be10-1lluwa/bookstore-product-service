ebf03293ea5b1365e8f75409bf602227c0fb5e2c
JunHyeon <tempjun0215@gmail.com>
2025-06-18 09:39:40 +0900

    feat: ReviewLike Controller 작성

---
+++ src/main/java/com/nhnacademy/illuwa/d_review/reviewlike/controller/ReviewLikeController.java
@@ -0,0 +1,37 @@
+package com.nhnacademy.illuwa.d_review.reviewlike.controller;
+
+import com.nhnacademy.illuwa.d_review.reviewlike.dto.ReviewLikeResponse;
+import com.nhnacademy.illuwa.d_review.reviewlike.service.ReviewLikeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/books/{bookId}/reviews/{reviewId}/like")
+public class ReviewLikeController {
+    private final ReviewLikeService reviewLikeService;
+
+    // TODO: memberId 가져오는 방법 결정되면 수정
+
+    @PostMapping
+    public ResponseEntity<ReviewLikeResponse> addLike(@PathVariable Long bookId, @PathVariable Long reviewId) {
+        Long memberId = null;
+
+        return ResponseEntity.ok(reviewLikeService.addLike(bookId, reviewId, memberId));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<ReviewLikeResponse> cancelLike(@PathVariable Long bookId, @PathVariable Long reviewId) {
+        Long memberId = null;
+
+        return ResponseEntity.ok(reviewLikeService.cancelLike(bookId, reviewId, memberId));
+    }
+
+    @GetMapping
+    public ResponseEntity<ReviewLikeResponse> getLikeInfo(@PathVariable Long bookId, @PathVariable Long reviewId) {
+        Long memberId = null;
+
+        return ResponseEntity.ok(reviewLikeService.getLikeInfo(bookId, reviewId, memberId));
+    }
+}
