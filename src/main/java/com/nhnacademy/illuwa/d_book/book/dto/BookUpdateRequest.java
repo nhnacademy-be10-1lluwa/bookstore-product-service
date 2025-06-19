a0b3597a34284cd1a6ca1906badafe3e72a7c126
nhdor <ch@>
2025-06-19 19:35:33 +0900

    feat : #95 생성자 애노테이션 추가

--- src/main/java/com/nhnacademy/illuwa/d_book/book/dto/BookUpdateRequest.java
+++ src/main/java/com/nhnacademy/illuwa/d_book/book/dto/BookUpdateRequest.java
@@ -1,11 +1,15 @@
 package com.nhnacademy.illuwa.d_book.book.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookUpdateRequest {
 
     private String contents;
