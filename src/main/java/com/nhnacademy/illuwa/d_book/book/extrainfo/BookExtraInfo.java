12fa5304d2ab55e9322628b4341002c5de02fc73
nhdor <ch@>
2025-06-20 18:19:13 +0900

    feat : bookExtraInfo 추가

---
+++ src/main/java/com/nhnacademy/illuwa/d_book/book/extrainfo/BookExtraInfo.java
@@ -0,0 +1,22 @@
+package com.nhnacademy.illuwa.d_book.book.extrainfo;
+
+import com.nhnacademy.illuwa.common.enums.Status;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class BookExtraInfo {
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    private boolean giftwrap;
+
+}
