4502a749786ecda1acf3b71cb14cd4635717be36
nhdor <ch@>
2025-06-21 19:20:55 +0900

    refactor : #111 - 태그 등록 시 응답( id, name )

---
+++ src/main/java/com/nhnacademy/illuwa/d_book/tag/dto/TagResponse.java
@@ -0,0 +1,11 @@
+package com.nhnacademy.illuwa.d_book.tag.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@AllArgsConstructor
+public class TagResponse {
+    private Long id;
+    private String name;
+}
