464944741b38ec67808f7514fd21aa50b7447f6c
nhdor <ch@>
2025-06-21 14:26:21 +0900

    feat : tag dto 추가

---
+++ src/main/java/com/nhnacademy/illuwa/d_book/tag/dto/TagRegisterRequest.java
@@ -0,0 +1,14 @@
+package com.nhnacademy.illuwa.d_book.tag.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class TagRegisterRequest {
+    private String name;
+}
