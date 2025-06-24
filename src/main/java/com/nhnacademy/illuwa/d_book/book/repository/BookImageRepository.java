c547f00c5b20feb133591000051d542c77922069
nhdor <ch@>
2025-06-24 19:07:03 +0900

    feat : #130 BookImageRepository 추가

---
+++ src/main/java/com/nhnacademy/illuwa/d_book/book/repository/BookImageRepository.java
@@ -0,0 +1,7 @@
+package com.nhnacademy.illuwa.d_book.book.repository;
+
+import com.nhnacademy.illuwa.d_book.book.entity.BookImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookImageRepository extends JpaRepository<BookImage,Long> {
+}
