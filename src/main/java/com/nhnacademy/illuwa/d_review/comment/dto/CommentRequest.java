9ee31e608014761e4f9ef88bb3b4cccb7255e0a0
JunHyeon <tempjun0215@gmail.com>
2025-06-24 15:38:14 +0900

    refactor: 경로 이름 수정, NoArgsConstructor 필요해서 다시 추가

--- src/main/java/com/nhnacademy/illuwa/d_review/comment/dto/CommentRequest.java
+++ src/main/java/com/nhnacademy/illuwa/d_review/comment/dto/CommentRequest.java
@@ -2,8 +2,10 @@ package com.nhnacademy.illuwa.d_review.comment.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class CommentRequest {
     String commentContents;
