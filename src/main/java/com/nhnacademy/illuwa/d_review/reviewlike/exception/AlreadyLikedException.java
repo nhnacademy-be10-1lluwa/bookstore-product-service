ebd50ba32aadc732e09887755c4c4ad0e25ee69f
JunHyeon <tempjun0215@gmail.com>
2025-06-24 17:00:34 +0900

    cleanup: 필요없는 예외 삭제

--- src/main/java/com/nhnacademy/illuwa/d_review/reviewlike/exception/AlreadyLikedException.java
+++
@@ -1,7 +0,0 @@
-package com.nhnacademy.illuwa.d_review.reviewlike.exception;
-
-public class AlreadyLikedException extends RuntimeException {
-    public AlreadyLikedException() {
-        super("좋아요를 여러 번 설정 할 수 없습니다.");
-    }
-}
