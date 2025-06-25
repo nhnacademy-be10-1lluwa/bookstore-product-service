976ea5bd65237e59e1ff685707d1d0e752804c02
aaaaaattt <ch@>
2025-06-25 16:09:44 +0900

    feat : #132 RegisterRequest dto 수정 (form에서 직접 등록하도록)

--- src/main/java/com/nhnacademy/illuwa/d_book/book/dto/BookRegisterRequest.java
+++ src/main/java/com/nhnacademy/illuwa/d_book/book/dto/BookRegisterRequest.java
@@ -1,13 +1,32 @@
 package com.nhnacademy.illuwa.d_book.book.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class BookRegisterRequest {
+
+    @NotBlank
+    private String title;
+    private String contents;
+    private String description;
+    @NotBlank
+    private String author;
+    @NotBlank
+    private String publisher;
+    private LocalDate pubDate;
     @NotBlank
     private String isbn;
+    private int regularPrice;
+    private int salePrice;
+    @NotBlank
+    @JsonProperty("cover")
+    private String imgUrl;
 }
