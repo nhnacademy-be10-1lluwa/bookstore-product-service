c05fcf483c718559d887a3323ae6644c91830c9d
aaaaaattt <ch@>
2025-06-27 01:21:38 +0900

    clean-up : #145 중복 repository 삭제

--- src/main/java/com/nhnacademy/illuwa/d_book/book/repository/BookCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nhnacademy.illuwa.d_book.book.repository;
-
-import com.nhnacademy.illuwa.d_book.category.entity.BookCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface BookCategoryRepository extends JpaRepository<BookCategory,Long> {
-
-}
