c559d968d030411648425ae57ccc096a4b174e03
JunHyeon <tempjun0215@gmail.com>
2025-06-26 16:11:28 +0900

    refactor: ReviewImage 업로드 방식 변경 (MinIO)

---
+++ src/main/java/com/nhnacademy/illuwa/d_review/review/repository/ReviewImageRepository.java
@@ -0,0 +1,7 @@
+package com.nhnacademy.illuwa.d_review.review.repository;
+
+import com.nhnacademy.illuwa.d_review.review.entity.ReviewImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewImageRepository extends JpaRepository<ReviewImage, Long> {
+}
