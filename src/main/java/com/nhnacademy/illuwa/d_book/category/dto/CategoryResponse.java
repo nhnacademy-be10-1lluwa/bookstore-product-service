b677a83c7e692a678d540dd74a8c8f7a1bbb6cc1
ch <chlgur13m@naver.com>
2025-06-28 02:48:53 +0900

    feat : #157 test -CategoryService -CategoryController

--- src/main/java/com/nhnacademy/illuwa/d_book/category/dto/CategoryResponse.java
+++ src/main/java/com/nhnacademy/illuwa/d_book/category/dto/CategoryResponse.java
@@ -2,9 +2,11 @@ package com.nhnacademy.illuwa.d_book.category.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
+@Setter
 public class CategoryResponse {
     private Long id;
     private Long parentCategoryId;
