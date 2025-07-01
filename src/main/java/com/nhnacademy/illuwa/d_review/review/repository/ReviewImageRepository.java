ae57884e40e687dbfef523557fc26dac7a8dedaa
JunHyeon <tempjun0215@gmail.com>
2025-07-01 10:50:56 +0900

    feat: 리뷰 작성, 수정시 이미지 업로드/삭제 기능 구현

--- src/main/java/com/nhnacademy/illuwa/d_review/review/repository/ReviewImageRepository.java
+++ src/main/java/com/nhnacademy/illuwa/d_review/review/repository/ReviewImageRepository.java
@@ -3,5 +3,8 @@ package com.nhnacademy.illuwa.d_review.review.repository;
 import com.nhnacademy.illuwa.d_review.review.entity.ReviewImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewImageRepository extends JpaRepository<ReviewImage, Long> {
+    List<ReviewImage> findAllByReview_ReviewId(Long reviewId);
 }
