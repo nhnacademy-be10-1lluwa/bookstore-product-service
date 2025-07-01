e3662205bdc790d306a692852243fd924d950c3d
{haini000} <{haini00)@naver.com}>
2025-07-01 13:19:07 +0900

    fix: service 수정

--- src/main/java/com/nhnacademy/illuwa/cart/service/CartService.java
+++ src/main/java/com/nhnacademy/illuwa/cart/service/CartService.java
@@ -1,27 +1,45 @@
 package com.nhnacademy.illuwa.cart.service;
 
-import com.nhnacademy.illuwa.cart.entity.BookCart;
+import com.nhnacademy.illuwa.cart.dto.BookCartRequest;
+import com.nhnacademy.illuwa.cart.dto.BookCartResponse;
+import com.nhnacademy.illuwa.cart.dto.CartRequest;
+import com.nhnacademy.illuwa.cart.dto.CartResponse;
 import com.nhnacademy.illuwa.cart.entity.Cart;
-
-import java.util.List;
 
 public interface CartService {
 
-    // 회원 번호로 카트 조회 -> 없으면 생성
-    Cart getOrCreateCart(Long memberId);
+//    // 회원 번호로 카트 조회 -> 없으면 생성
+//    Cart getOrCreateCart(Long memberId);
+//
+//    // 도서를 추가하거나 기존 항목 수량 합산 -> 제구 부족시 오류
+//    BookCart addItem(Long memberId, Long bookId, int amount);
+//
+//    // 수량 변경
+//    BookCart updateItem(Long memberId, Long bookId, int amount);
+//
+//    // 특정 책 제거
+//    void removeItem(Long memberId, Long bookId);
+//
+//    // 카트 내 모든 항목 제거
+//    List<BookCart> getCartItems(Long memberId);
+//
+//    // 결제 후 장바구니 비우기
+//    void cleanCart(Long memberId);
 
-    // 도서를 추가하거나 기존 항목 수량 합산 -> 제구 부족시 오류
-    BookCart addItem(Long memberId, Long bookId, int amount);
+    // 장바구니 조회
+    Cart getOrCreateCart(CartRequest request);
 
-    // 수량 변경
-    BookCart updateItem(Long memberId, Long bookId, int amount);
+    // 장바구니 내용 조회
+    CartResponse getCart(CartRequest Request);
 
-    // 특정 책 제거
-    void removeItem(Long memberId, Long bookId);
+    // 도서 추가
+    BookCartResponse addBook(BookCartRequest request);
 
-    // 카트 내 모든 항목 제거
-    List<BookCart> getCartItems(Long memberId);
+    // 도서 수량 수정
+    BookCartResponse updateBookCart(BookCartRequest request);
 
-    // 결제 후 장바구니 비우기
-    void cleanCart(Long memberId);
+    //당일 ㅈ
+    void removeBookCart(BookCartRequest request);
+
+    void cleanCart(CartRequest request);
 }
