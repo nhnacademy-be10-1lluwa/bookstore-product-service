22a53f9351aa42a37d75b7f6eedbc32851c9d1c5
nhdor <ch@>
2025-07-02 22:24:24 +0900

    cleanup(category): 불필요한 코드 제거

--- src/main/java/com/nhnacademy/illuwa/d_book/category/repository/category/CustomizedCategoryRepository.java
+++ src/main/java/com/nhnacademy/illuwa/d_book/category/repository/category/CustomizedCategoryRepository.java
@@ -6,7 +6,4 @@ import java.util.Optional;
 
 public interface CustomizedCategoryRepository {
 
-
-//    카테고리는 도서 등록할 때마다 저장되는 게 아니라 미리 .sql 파일로  setting 예정
-//    Optional<Category> findByCategoryNameAndParentCategory(String categoryName, Category parentCategory);
 }
