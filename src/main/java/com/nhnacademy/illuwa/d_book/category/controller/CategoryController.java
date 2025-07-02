72bece688ea25569e2eb5a7ec016fc5fda59a497
nhdor <chlgur13m@naver.com>
2025-07-02 22:36:26 +0900

    [CHORE]/category-불필요한 코드 삭제 (#180)
    * cleanup(category): 불필요한 코드 제거
    
    * chore(category): 불필요한 코드 삭제
    
    ---------
    
    Co-authored-by: nhdor <ch@>

--- src/main/java/com/nhnacademy/illuwa/d_book/category/controller/CategoryController.java
+++ src/main/java/com/nhnacademy/illuwa/d_book/category/controller/CategoryController.java
@@ -2,12 +2,8 @@ package com.nhnacademy.illuwa.d_book.category.controller;
 
 import com.nhnacademy.illuwa.d_book.category.dto.CategoryResponse;
 import com.nhnacademy.illuwa.d_book.category.service.CategoryService;
-import jakarta.ws.rs.Path;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,13 +18,12 @@ public class CategoryController {
 
     @GetMapping()
     public ResponseEntity<List<CategoryResponse>> getAllCategories(){
-        List<CategoryResponse> allCategories = categoryService.getAllCategories();
-        return ResponseEntity.ok(allCategories);
+        return ResponseEntity.ok(categoryService.getAllCategories());
     }
 
     @GetMapping("/{categoryId}")
     public ResponseEntity<CategoryResponse> getCategoryInfo(@PathVariable Long categoryId) {
-        CategoryResponse categoryInfo = categoryService.getCategoryInfo(categoryId);
-        return ResponseEntity.ok(categoryInfo);
+        return ResponseEntity.ok(categoryService.getCategoryInfo(categoryId));
     }
+
 }
--- src/main/java/com/nhnacademy/illuwa/d_book/category/repository/category/CustomizedCategoryRepository.java
+++ src/main/java/com/nhnacademy/illuwa/d_book/category/repository/category/CustomizedCategoryRepository.java
@@ -1,12 +1,6 @@
 package com.nhnacademy.illuwa.d_book.category.repository.category;
 
-import com.nhnacademy.illuwa.d_book.category.entity.Category;
-
-import java.util.Optional;
 
 public interface CustomizedCategoryRepository {
 
-
-//    카테고리는 도서 등록할 때마다 저장되는 게 아니라 미리 .sql 파일로  setting 예정
-//    Optional<Category> findByCategoryNameAndParentCategory(String categoryName, Category parentCategory);
 }
