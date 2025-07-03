8e30cabb46153707d5d1b48ce54b19af855c0efd
nhdor <ch@>
2025-07-03 13:36:13 +0900

    feat(tag): 도서 태그 카테고리 페이지네이션 추가 #188

--- src/main/java/com/nhnacademy/illuwa/d_book/tag/controller/AdminTagController.java
+++ src/main/java/com/nhnacademy/illuwa/d_book/tag/controller/AdminTagController.java
@@ -2,16 +2,19 @@ package com.nhnacademy.illuwa.d_book.tag.controller;
 
 import com.nhnacademy.illuwa.d_book.tag.dto.TagRegisterRequest;
 import com.nhnacademy.illuwa.d_book.tag.dto.TagResponse;
+import com.nhnacademy.illuwa.d_book.tag.entity.Tag;
 import com.nhnacademy.illuwa.d_book.tag.repository.TagRepository;
 import com.nhnacademy.illuwa.d_book.tag.service.TagService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
 
 @RestController
-@RequestMapping("/admin/tags")
+@RequestMapping("/tags")
 public class AdminTagController {
 
     TagService tagService;
@@ -25,10 +28,13 @@ public class AdminTagController {
 
     @PostMapping()
     public ResponseEntity<TagResponse> registerTag(@RequestBody @Valid TagRegisterRequest tagRegisterRequest){
-
-
-
         return ResponseEntity.ok(tagService.registerTag(tagRegisterRequest));
+    }
+
+    @GetMapping()
+    public ResponseEntity<Page<TagResponse>> getAllTags(Pageable pageable){
+        Page<TagResponse> allTags = tagService.getAllTags(pageable).map(t->new TagResponse(t.getId(),t.getName()));
+        return ResponseEntity.ok(allTags);
     }
 
     @DeleteMapping("/{id}")
