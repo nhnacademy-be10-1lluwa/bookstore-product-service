5e62dd8ef38d78a1aa5212b7c22e8147829fd4c9
nhdor <ch@>
2025-07-05 21:49:50 +0900

    feat(book):200 Book 도메인에 QueryDSL추가

---
+++ src/main/java/com/nhnacademy/illuwa/d_book/book/repository/BookRepositoryCustom.java
@@ -0,0 +1,10 @@
+package com.nhnacademy.illuwa.d_book.book.repository;
+
+import com.nhnacademy.illuwa.d_book.book.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface BookRepositoryCustom {
+
+    Page<Book> findBooksByCriteria(Long categoryId, String tagName, Pageable pageable);
+}
