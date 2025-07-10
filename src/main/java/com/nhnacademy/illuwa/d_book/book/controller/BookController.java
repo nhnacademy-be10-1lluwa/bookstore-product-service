908670e2915886b03f7cb623a50eb8a212b22ddd
nhdor <ch@>
2025-07-10 23:53:40 +0900

    feat(book): API 엔드포인트 추가(외부 api 도서 검색 by isbn)

--- src/main/java/com/nhnacademy/illuwa/d_book/book/controller/BookController.java
+++ src/main/java/com/nhnacademy/illuwa/d_book/book/controller/BookController.java
@@ -3,6 +3,7 @@ package com.nhnacademy.illuwa.d_book.book.controller;
 import com.nhnacademy.illuwa.d_book.book.document.BookDocument;
 import com.nhnacademy.illuwa.d_book.book.dto.response.BestSellerResponse;
 import com.nhnacademy.illuwa.d_book.book.dto.response.BookDetailResponse;
+import com.nhnacademy.illuwa.d_book.book.dto.response.BookExternalResponse;
 import com.nhnacademy.illuwa.d_book.book.mapper.BookMapper;
 import com.nhnacademy.illuwa.d_book.book.service.BookService;
 import com.nhnacademy.illuwa.infra.apiclient.AladinBookApiService;
@@ -92,6 +93,17 @@ public class BookController {
     public ResponseEntity<BookDetailResponse> searchBookByIsbnInDB(@PathVariable String isbn){
         BookDetailResponse bookByIsbn = bookService.searchBookByIsbn(isbn);
         return ResponseEntity.ok(bookByIsbn);
+    }
+
+    @GetMapping("/external/isbn/{isbn}")
+    public ResponseEntity<BookExternalResponse> getBookByIsbnFromExternalApi(@PathVariable String isbn){
+        BookExternalResponse bookDetail = aladinBookApiService.findBookByIsbn(isbn);
+
+        if (bookDetail == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(bookDetail);
     }
 
 
