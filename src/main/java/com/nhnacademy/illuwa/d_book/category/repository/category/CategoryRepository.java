f7c54fa450f8a35d153e6658b41306e66f626ba2
nhdor <ch@>
2025-07-10 09:36:12 +0900

    feat(cateogry): root 계층의 카테고리 불러오는 api 추가

--- src/main/java/com/nhnacademy/illuwa/d_book/category/repository/category/CategoryRepository.java
+++ src/main/java/com/nhnacademy/illuwa/d_book/category/repository/category/CategoryRepository.java
@@ -3,5 +3,8 @@ package com.nhnacademy.illuwa.d_book.category.repository.category;
 import com.nhnacademy.illuwa.d_book.category.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> , CustomizedCategoryRepository{
+    List<Category> findByParentCategoryIsNull();
 }
