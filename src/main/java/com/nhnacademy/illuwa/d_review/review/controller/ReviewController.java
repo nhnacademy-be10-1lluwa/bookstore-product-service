fe28e44e08314d73a858cbf24b9248e00ee55330
JunHyeon <tempjun0215@gmail.com>
2025-07-11 19:11:47 +0900

    refactor: modelattribute 사용

--- src/main/java/com/nhnacademy/illuwa/d_review/review/controller/ReviewController.java
+++ src/main/java/com/nhnacademy/illuwa/d_review/review/controller/ReviewController.java
@@ -5,6 +5,7 @@ import com.nhnacademy.illuwa.d_review.review.dto.ReviewResponse;
 import com.nhnacademy.illuwa.d_review.review.service.ReviewService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -16,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
-
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/books/{bookId}/reviews")
@@ -24,18 +25,17 @@ public class ReviewController {
     private final ReviewService reviewService;
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<ReviewResponse> createReview(@PathVariable Long bookId,
-                                                       @RequestPart("review") @Valid ReviewRequest request,
-                                                       @RequestHeader("X-USER-ID") Long memberId,
-                                                       @RequestPart(name = "images", required = false) List<MultipartFile> images) throws Exception {
-
-        ReviewResponse response = reviewService.createReview(bookId, request, memberId, images);
+    public ResponseEntity<ReviewResponse> createReview(@PathVariable long bookId,
+                                                       @RequestHeader("X-USER-ID") long memberId,
+                                                       @ModelAttribute ReviewRequest request) throws Exception {
+        log.info("{} // {} // {} // {}", bookId, request.getReviewTitle(), request.getReviewContent(), request.getReviewRating());
+        ReviewResponse response = reviewService.createReview(bookId, memberId, request, request.getImages());
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping
-    public ResponseEntity<Page<ReviewResponse>> getReviewPages(@PathVariable Long bookId,
-                                                               @RequestHeader("X-USER-ID") Long memberId,
+    public ResponseEntity<Page<ReviewResponse>> getReviewPages(@PathVariable long bookId,
+                                                               @RequestHeader("X-USER-ID") long memberId,
                                                                @PageableDefault(size = 5, sort = "reviewDate", direction = Sort.Direction.DESC) Pageable pageable) {
 
         Page<ReviewResponse> responsePage = reviewService.getReviewPages(bookId, pageable, memberId);
@@ -43,22 +43,22 @@ public class ReviewController {
     }
 
     @GetMapping(value = "/{reviewId}")
-    public ResponseEntity<ReviewResponse> getReviewDetails(@PathVariable Long bookId,
-                                                           @PathVariable Long reviewId,
-                                                           @RequestHeader("X-USER-ID") Long memberId) {
+    public ResponseEntity<ReviewResponse> getReviewDetails(@PathVariable long bookId,
+                                                           @PathVariable long reviewId,
+                                                           @RequestHeader("X-USER-ID") long memberId) {
         ReviewResponse response = reviewService.getReviewDetails(bookId, reviewId, memberId);
         return ResponseEntity.ok(response);
     }
 
     @PatchMapping(value = "/{reviewId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<ReviewResponse> updateReview(@PathVariable Long bookId,
-                                                       @PathVariable Long reviewId,
-                                                       @RequestPart("review") @Valid ReviewRequest request,
+    public ResponseEntity<ReviewResponse> updateReview(@PathVariable long bookId,
+                                                       @PathVariable long reviewId,
                                                        @RequestHeader("X-USER-ID") Long memberId,
+                                                       @ModelAttribute ReviewRequest request,
                                                        @RequestPart(name = "images", required = false) List<MultipartFile> images,
                                                        @RequestPart(name = "keepImageUrls", required = false) List<String> keepImageUrls) throws Exception {
-
-        ReviewResponse response = reviewService.updateReview(bookId, reviewId, request, memberId, images, keepImageUrls);
+        log.info("{} // {} // {} // {}", bookId, request.getReviewTitle(), request.getReviewContent(), request.getReviewRating());
+        ReviewResponse response = reviewService.updateReview(bookId, reviewId, memberId, request, images, keepImageUrls);
         return ResponseEntity.ok(response);
     }
 }
