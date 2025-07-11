64f1b41cc8118bddcfa886d4bf5fa5a2b9a15065
RiveroadKim <kanggil0825@gmail.com>
2025-07-11 15:33:36 +0900

    refact: application 이름 변경

--- src/test/java/com/nhnacademy/illuwa/ProductServiceApplicationTests.java
+++ src/test/java/com/nhnacademy/illuwa/ProductServiceApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class IlluwaApplicationTests {
+class ProductServiceApplicationTests {
 
     @Test
     void contextLoads() {
