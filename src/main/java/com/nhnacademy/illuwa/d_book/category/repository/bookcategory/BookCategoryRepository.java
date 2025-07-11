bb9da1e4bd9f9030609a63a89563d1befb53b807
nhdor <ch@>
2025-07-12 08:16:06 +0900

    fix(book) : 도서id를 통해 도서_카테고리 조회

--- src/main/java/com/nhnacademy/illuwa/d_book/category/repository/bookcategory/BookCategoryRepository.java
+++ src/main/java/com/nhnacademy/illuwa/d_book/category/repository/bookcategory/BookCategoryRepository.java
@@ -3,5 +3,8 @@ package com.nhnacademy.illuwa.d_book.category.repository.bookcategory;
 import com.nhnacademy.illuwa.d_book.category.entity.BookCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookCategoryRepository extends JpaRepository<BookCategory, Long>, CustomizedBookCategoryRepository {
+    Optional<BookCategory> findByBookId(Long bookId);
 }
