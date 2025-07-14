21a6bae1f3a7b62f142102eb111fe0b301bc3774
nhdor <ch@>
2025-07-14 18:04:58 +0900

    fix(tag): 태그 api 테스트 수정

--- src/test/java/com/nhnacademy/illuwa/d_book/tag/service/TagServiceTest.java
+++ src/test/java/com/nhnacademy/illuwa/d_book/tag/service/TagServiceTest.java
@@ -2,28 +2,36 @@ package com.nhnacademy.illuwa.d_book.tag.service;
 
 import com.nhnacademy.illuwa.d_book.tag.dto.TagRegisterRequest;
 import com.nhnacademy.illuwa.d_book.tag.dto.TagResponse;
+import com.nhnacademy.illuwa.d_book.tag.entity.BookTag;
 import com.nhnacademy.illuwa.d_book.tag.entity.Tag;
+import com.nhnacademy.illuwa.d_book.tag.repository.BookTagRepository;
 import com.nhnacademy.illuwa.d_book.tag.repository.TagRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class TagServiceTest {
 
     @Mock
     TagRepository tagRepository;
+
+    @Mock
+    BookTagRepository bookTagRepository;
+
 
     @InjectMocks
     TagService tagService;
@@ -51,13 +59,28 @@ class TagServiceTest {
     }
 
     @Test
+    @DisplayName("태그 삭제 - 성공")
     void deleteTag() {
-
         Long tagId = 11L;
+
+        Tag tag = new Tag();
+        tag.setId(tagId);
+        tag.setName("Test Tag");
+
+        List<BookTag> bookTags = List.of(
+                mock(BookTag.class),
+                mock(BookTag.class)
+        );
+
+        when(tagRepository.findById(tagId)).thenReturn(Optional.of(tag));
+
+        when(bookTagRepository.findByTagId(tagId)).thenReturn(bookTags);
 
         tagService.deleteTag(tagId);
 
-        verify(tagRepository).deleteById(tagId);
-
+        verify(tagRepository, times(1)).findById(tagId);
+        verify(bookTagRepository, times(1)).findByTagId(tagId);
+        verify(bookTagRepository, times(1)).deleteAll(bookTags);
+        verify(tagRepository, times(1)).delete(tag);
     }
 }
