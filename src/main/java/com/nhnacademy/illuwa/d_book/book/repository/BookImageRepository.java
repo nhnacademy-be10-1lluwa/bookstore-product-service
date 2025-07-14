fa23c7c15e3b93a0c1f10d80cf6b3bf31a00c0b5
nhdor <ch@>
2025-07-14 15:45:44 +0900

    feat(bookImage): 도서 이미지 검색 api 추가(ImageType)

--- src/main/java/com/nhnacademy/illuwa/d_book/book/repository/BookImageRepository.java
+++ src/main/java/com/nhnacademy/illuwa/d_book/book/repository/BookImageRepository.java
@@ -1,8 +1,14 @@
 package com.nhnacademy.illuwa.d_book.book.repository;
 
 import com.nhnacademy.illuwa.d_book.book.entity.BookImage;
+import com.nhnacademy.illuwa.d_book.book.enums.ImageType;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface BookImageRepository extends JpaRepository<BookImage,Long> {
     void deleteAllByBook_Id(Long bookId);
+    List<BookImage> findByBookId(Long bookId);
+    Optional<BookImage> findByBookIdAndImageType(Long bookId, ImageType imageType);
 }
