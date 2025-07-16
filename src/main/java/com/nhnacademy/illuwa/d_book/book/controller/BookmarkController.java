4189a22937e262feef8ae7d2d536b699472ea72b
JunHyeon <tempjun0215@gmail.com>
2025-07-16 21:41:55 +0900

    fix: controller 매핑 변경

--- src/main/java/com/nhnacademy/illuwa/d_book/book/controller/BookmarkController.java
+++ src/main/java/com/nhnacademy/illuwa/d_book/book/controller/BookmarkController.java
@@ -1,35 +1,21 @@
 package com.nhnacademy.illuwa.d_book.book.controller;
 
-import com.nhnacademy.illuwa.d_book.book.dto.response.BookDetailResponse;
 import com.nhnacademy.illuwa.d_book.book.dto.response.BookmarkResponse;
-import com.nhnacademy.illuwa.d_book.book.service.BookService;
 import com.nhnacademy.illuwa.d_book.book.service.BookmarkService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/user/books/{isbn}/bookmarks")
+@RequestMapping("/api/bookmarks")
 public class BookmarkController {
-    private final BookService bookService;
     private final BookmarkService bookmarkService;
 
     @PostMapping
-    public ResponseEntity<BookmarkResponse> toggleBookmark(@PathVariable String isbn,
+    public ResponseEntity<BookmarkResponse> toggleBookmark(@RequestParam long bookId,
                                                            @RequestHeader("X-USER-ID") Long memberId) {
 
-        Optional<BookDetailResponse> bookInfo = bookService.getAllBooks().stream()
-                .filter(book -> book.getIsbn().equals(isbn))
-                .findFirst();
-
-        if (bookInfo.isPresent()) {
-            long bookId = bookInfo.get().getId();
-            return ResponseEntity.ok(bookmarkService.toggleBookmark(bookId, memberId));
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return ResponseEntity.ok(bookmarkService.toggleBookmark(bookId, memberId));
     }
 }
