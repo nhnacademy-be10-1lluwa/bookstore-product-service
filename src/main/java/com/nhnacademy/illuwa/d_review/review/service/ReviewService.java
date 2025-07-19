b2fd9705ecfab6655c2ab4feb03400c3591d8838
JunHyeon <tempjun0215@gmail.com>
2025-07-20 06:58:50 +0900

    cleanup: 안쓰는 import 삭제

--- src/main/java/com/nhnacademy/illuwa/d_review/review/service/ReviewService.java
+++ src/main/java/com/nhnacademy/illuwa/d_review/review/service/ReviewService.java
@@ -2,7 +2,6 @@ package com.nhnacademy.illuwa.d_review.review.service;
 
 import com.nhnacademy.illuwa.d_review.review.dto.ReviewRequest;
 import com.nhnacademy.illuwa.d_review.review.dto.ReviewResponse;
-import com.nhnacademy.illuwa.d_review.review.entity.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
