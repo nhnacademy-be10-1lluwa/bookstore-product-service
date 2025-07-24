c8a3ab2eef9eea26bcbda09bf7567cea96770263
nhdor <ch@>
2025-07-24 11:14:32 +0900

    feat: Tag Controller Swagger 적용

--- src/main/java/com/nhnacademy/illuwa/d_book/tag/controller/TagController.java
+++ src/main/java/com/nhnacademy/illuwa/d_book/tag/controller/TagController.java
@@ -2,6 +2,12 @@ package com.nhnacademy.illuwa.d_book.tag.controller;
 
 import com.nhnacademy.illuwa.d_book.tag.dto.TagResponse;
 import com.nhnacademy.illuwa.d_book.tag.service.TagService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "태그 API", description = "태그 조회 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/tags")
@@ -17,6 +24,11 @@ public class TagController {
 
     private final TagService tagService;
 
+    @Operation(summary = "모든 태그 조회", description = "모든 태그 목록을 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "태그 목록 조회 성공",
+                    content = @Content(schema = @Schema(implementation = TagResponse.class)))
+    })
     @GetMapping
     public ResponseEntity<List<TagResponse>> getAllTags() {
         return ResponseEntity.ok(tagService.getAllTags());
