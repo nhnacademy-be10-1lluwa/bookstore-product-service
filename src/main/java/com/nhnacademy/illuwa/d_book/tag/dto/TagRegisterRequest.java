c405875305b67fcbedf16f8fdc60c8bdbddc053f
nhdor <ch@>
2025-07-24 13:07:44 +0900

    feat: 태그 등록 dto 스웨거 등록

--- src/main/java/com/nhnacademy/illuwa/d_book/tag/dto/TagRegisterRequest.java
+++ src/main/java/com/nhnacademy/illuwa/d_book/tag/dto/TagRegisterRequest.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.illuwa.d_book.tag.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,8 +12,10 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "태그 등록 요청 DTO")
 public class TagRegisterRequest {
 
     @NotBlank(message = "태그 이름은 필수입니다.")
+    @Schema(description = "태그 이름", example = "소설")
     private String name;
 }
