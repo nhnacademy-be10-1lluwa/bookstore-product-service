7db7b39c820b406db8574c00b36ed8ee44639745
nhdor <ch@>
2025-07-24 11:20:57 +0900

    feat: 카테고리 response dto 스웨거 등록

--- src/main/java/com/nhnacademy/illuwa/d_book/category/dto/CategoryFlatResponse.java
+++ src/main/java/com/nhnacademy/illuwa/d_book/category/dto/CategoryFlatResponse.java
@@ -1,20 +1,25 @@
 package com.nhnacademy.illuwa.d_book.category.dto;
 
-import lombok.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
-@AllArgsConstructor
+@Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "평면 카테고리 응답 DTO")
 public class CategoryFlatResponse {
+    @Schema(description = "카테고리 ID", example = "1")
     private Long id;
+    @Schema(description = "부모 카테고리 ID", example = "null")
     private Long parentId;
+    @Schema(description = "부모 카테고리 이름", example = "null")
     private String parentName;
+    @Schema(description = "카테고리 이름", example = "소설")
     private String categoryName;
+    @Schema(description = "카테고리 깊이 (0부터 시작)", example = "0")
     private int depth;
-
-    public CategoryFlatResponse(Long id, Long parentId, String categoryName) {
-        this.id = id;
-        this.parentId = parentId;
-        this.categoryName = categoryName;
-    }
 }
