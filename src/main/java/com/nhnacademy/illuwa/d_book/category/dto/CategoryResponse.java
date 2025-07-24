a4c4dc2e10b0ec893b90fb65b5ab9ca46461c5c8
nhdor <ch@>
2025-07-24 13:07:38 +0900

    feat: BookCategory response dto 스웨거 등록

--- src/main/java/com/nhnacademy/illuwa/d_book/category/dto/CategoryResponse.java
+++ src/main/java/com/nhnacademy/illuwa/d_book/category/dto/CategoryResponse.java
@@ -1,6 +1,7 @@
 package com.nhnacademy.illuwa.d_book.category.dto;
 
 import com.nhnacademy.illuwa.d_book.category.entity.Category;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.util.ArrayList;
@@ -12,33 +13,31 @@ import java.util.stream.Collectors;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Schema(description = "카테고리 응답 DTO")
 public class CategoryResponse {
+    @Schema(description = "카테고리 ID", example = "1")
     private Long id;
+    @Schema(description = "부모 카테고리 ID", example = "null")
     private Long parentId;
+    @Schema(description = "카테고리 이름", example = "소설")
     private String categoryName;
+    @Schema(description = "하위 카테고리 목록")
     private List<CategoryResponse> children = new ArrayList<>();
 
     public CategoryResponse(Category category) {
         this.id = category.getId();
+        this.parentId = (category.getParentCategory() != null) ? category.getParentCategory().getId() : null;
         this.categoryName = category.getCategoryName();
-        if (category.getParentCategory() != null) {
-            this.parentId = category.getParentCategory().getId();
-        }
-
-        if (category.getChildrenCategory() != null) {
-            this.children = category.getChildrenCategory().stream()
-                    .map(CategoryResponse::new)
-                    .collect(Collectors.toList());
-        }
+        this.children = category.getChildrenCategory().stream()
+                .map(CategoryResponse::new)
+                .collect(Collectors.toList());
     }
 
     public static CategoryResponse fromEntity(Category category) {
         return CategoryResponse.builder()
                 .id(category.getId())
-                .categoryName(category.getCategoryName())
                 .parentId(category.getParentCategory() != null ? category.getParentCategory().getId() : null)
+                .categoryName(category.getCategoryName())
                 .build();
     }
-
-
 }
